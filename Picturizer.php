6d3877636a9116684fed8ba2dfe1ac3494c1f3a6
laba <me@laba.net.ua>
2017-08-12 02:35:05 +0300

    add widget class

---
+++ Picturizer.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by Laba Mykola.
+ * Date: 12.08.17
+ * Time: 2:14
+ */
+
+namespace meliorator\picturizer;
+
+
+use yii\base\Widget;
+
+class Picturizer extends Widget {
+
+    public function run(){
+
+    }
+}
